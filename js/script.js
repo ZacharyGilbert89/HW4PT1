df3fb4a07e462e1363c30a1d975b3835144de095
Zach_Gilbert <zach.gilbert2020@gmail.com>
2023-11-29 14:33:56 -0500

    Converted to Part 1

--- js/script.js
+++ js/script.js
@@ -3,46 +3,15 @@ let minRow = 0;
 let maxCol = 0;
 let maxRow = 0;
 
-$(function(){
-  $("#minColSlider").slider({
-    min: -50,
-    max: 50,
-    slide: function (event, ui) {
-      $("#minCol").val(ui.value);
-      $("input").trigger("submit");
-    },
-  });
-  $("#maxColSlider").slider({
-    min: -50,
-    max: 50,
-    slide: function (event, ui) {
-      $("#maxCol").val(ui.value);
-
-      $("input").trigger("submit");
-    },
-  });
-  $("#minRowSlider").slider({
-    min: -50,
-    max: 50,
-    slide: function (event, ui) {
-      $("#minRow").val(ui.value);
-      $("input").trigger("submit");
-    },
-  });
-  $("#maxRowSlider").slider({
-    min: -50,
-    max: 50,
-    slide: function (event, ui) {
-      $("#maxRow").val(ui.value);
-      $("input").trigger("submit");
-    },
-  });
-});
 $.validator.addMethod("greaterThan", function(value, element, param){
   return this.optional(element) || parseInt(value) >= parseInt($(param).val());
 });
 $.validator.addMethod("noDecimal", function(value, element){
 return !(value % 1);
+});document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Form Submitted");
+  form.submit();
 });
 $(document).ready(function() 
 {
@@ -132,36 +101,6 @@ $(document).ready(function()
       tabledata.appendChild(table);
       //--------------------------------------------
     }
-  });
-});
-
-var num_tabs = $("tabs ul li").length + 1;
-$("#addTab").click(function(){
-  $("#tabs").tabs();
-  const elements = document.querySelectorAll('#tabs ul li');
-  const count = elements.length;
-  $("#tabs ul").append(
-    "<li><input type='checkbox'><a href='#tab-" + num_tabs + "'>TAB " + "</a><span class = 'ui-icon ui-icon-close'>Remove</span></li>"
-  );
-  $("#tabs").append('<div id="tab-' + num_tabs + '">' + $("#table").html() + "</div>");
-  num_tabs++;
-  $("#tabs").tabs("refresh");
-  $("#tabs").tabs({ active: (count - 1) });
-});
-var tabs = $("#tabs").tabs();
-tabs.delegate("span.ui-icon-close", "click", function() {
-  $("#tabs").tabs("refresh");
-  var panelId = $(this).closest("li").remove().attr("aria-controls");
-  $("#" + panelId).remove();
-  tabs.tabs("refresh");
-});
-$("#deleteSelected").on("click",function(){
-  $("#tabs").tabs("refresh");
-  $("input:checkbox").each(function() {
-      if ($(this).is(":checked")) {
-        var panelId = $(this).closest("li").remove().attr("aria-controls");
-        $("#" + panelId).remove();
-      }
   });
 });
 
