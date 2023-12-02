1556f8cb2f30f4e0068038dc43147d4e9705e0ac
Zach_Gilbert <zach.gilbert2020@gmail.com>
2023-12-02 00:41:58 -0500

    Added Comments

--- js/script.js
+++ js/script.js
@@ -1,20 +1,34 @@
+// File: script.js
+// GUI Assignment: HW3
+// Zachary Gilbert, UMass Lowell Computer Science,
+// Zachary_Gilbert@student.uml.edu
+// Copyright (c) 2023 by Zachary Gilbert. All rights reserved. May be freely copied or
+// excerpted for educational purposes with credit to the author.
+// updated by ZG on December 1, 2023 at 11:30 AM
+//global variables that hold the values for the table
 let minCol = 0;
 let minRow = 0;
 let maxCol = 0;
 let maxRow = 0;
 
-$.validator.addMethod("greaterThan", function(value, element, param){
+//validators
+
+$.validator.addMethod("greaterThan", function(value, element, param){ //makes sure that a min value doesnt exceed a max value
   return this.optional(element) || parseInt(value) >= parseInt($(param).val());
 });
-$.validator.addMethod("noDecimal", function(value, element){
-return !(value % 1);
-});document.querySelector("form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  alert("Form Submitted");
-  form.submit();
+
+$.validator.addMethod("noDecimal", function(value, element){//makes sure that the value entered is a number
+    return !(value % 1);
 });
+
+document.querySelector("form").addEventListener("submit", function (e) { //submit the form when submit button is pressed
+  e.preventDefault();
+  form.submit(); //submit form
+});
+
 $(document).ready(function() 
 {
+  //validations
   $("#form").validate({
     rules: {
       minCol: {
@@ -37,6 +51,7 @@ $(document).ready(function()
         greaterThan: "#minRow"
       }, 
     },
+    //Validation messages if the user creates an error
     messages: {
       minCol: {
           required: "Please Enter a Minimum Column",
@@ -61,44 +76,45 @@ $(document).ready(function()
         greaterThan: "Cannot have a minimum exceeding a maximum"
       }
     }, 
-    
     submitHandler: function(form) 
     {
       //Creates the TABLE
       const table = document.createElement("table");
       const tabledata = document.getElementById("table");
-      tabledata.innerHTML = "";
-
+      tabledata.innerHTML = ""; //clears previous table if there was one
+      //gets values
       minCol = document.getElementById("minCol").value;
       maxCol = document.getElementById("maxCol").value;
       minRow = document.getElementById("minRow").value;
       maxRow = document.getElementById("maxRow").value;
-      minRow -= 1;
-      minCol -= 1;
-
+      minRow -= 1; //offset
+      minCol -= 1; //offset
+      //fills table
+      minRow -= 1;//used this to offset so we can have a top row that shows each value
+      minCol -= 1;//used this to offset so we can have a left column that shows each column value
       for (let i = minRow; i <= maxRow; i++) {
-        row = document.createElement("tr");
-        for (let j = minCol; j <= maxCol; j++) {
+        row = document.createElement("tr");//creates a row
+        for (let j = minCol; j <= maxCol; j++) {//fills the row based on i*j
             colH = document.createElement("th");
-            if(i == minRow) {
-              if(i == minRow && j == minCol){
+            if(i == minRow) {//if its the first row
+              if(i == minRow && j == minCol){//if its the first cell in the table(top right cell), we do not want to do i*j, we instead want to fill it with nothing
                 colH.innerText = " ";
                 row.appendChild(colH);
-              } else {
+              } else { //since its the first row, we just want to store the j values
                 colH.innerText = j;
                 row.appendChild(colH);
               }
-            } else if(j == minCol) {
+            } else if(j == minCol) {//if its the first column, we only want to fill it with i values
               colH.innerText = i;
               row.appendChild(colH);
-            }else {
+            }else {//else it fills each cell with their respective products
               colH.innerText = j * i;
               row.appendChild(colH);
             }
         }
-        table.appendChild(row);
+        table.appendChild(row);//appends all the row data
       }
-      tabledata.appendChild(table);
+      tabledata.appendChild(table);//sets the table with the table data
       //--------------------------------------------
     }
   });
